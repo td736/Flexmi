358cc18929e1a6ff5c3c0464c08228a0eab80bde
Unknown <td736@york.ac.uk>
2018-04-08 02:54:09 +0100

    Added element field. Removed type field.

--- src/org/eclipse/epsilon/flexmi/FlexmiObject.java
+++ src/org/eclipse/epsilon/flexmi/FlexmiObject.java
@@ -2,23 +2,25 @@ package org.eclipse.epsilon.flexmi;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.Element;
+
 
 public class FlexmiObject {
 	
 	protected FlexmiObject parent;
 	protected List<FlexmiObject> children;
-	protected Object type;
+	protected Element element;
 	protected int depth;		// To be used in future to map elements by depth.
 	protected List<Object> possible_matches;  //@TODO Change type
 	protected boolean matched;  // True if the XML element has been matched to meta-model element. False otherwise.
 	protected Object match; 	// The meta-model element that this object has been mapped to.
 	
 	// Default Constructor
-	public FlexmiObject(FlexmiObject parent, int depth) {
+	public FlexmiObject(FlexmiObject parent, Element element, int depth) {
 		this.parent = parent;
 		this.children = new ArrayList<FlexmiObject>();
 		
-		this.type = null;
+		this.element = element;
 		this.depth = depth;
 		
 		this.possible_matches = new ArrayList<Object>();
@@ -27,8 +29,7 @@ public class FlexmiObject {
 	}
 	
 	// Constructor called when parent no parent exists
-	public FlexmiObject(int depth) { 
-		this(null, depth);
+	public FlexmiObject(Element element, int depth) { 
+		this(null, element, depth);
 	}
-
 }
